9cc558019bc0f3231df6e28225ad4980b30298b0
lopezmt <lpzmateo@gmail.com>
2019-06-18 11:45:23 -0400

    ENH: Register sample data

--- Modules/Scripted/Home/Home.py
+++ Modules/Scripted/Home/Home.py
@@ -82,6 +82,30 @@ The drop-down Modules are ordered to follow the basic workflow for choosing and 
         # SPACER
         self.layout.addStretch()
 
+
+        #SAMPLE DATA REGISTRATION
+        json_list = [self.resourcePath('SampleDataDescription/ShapeRegressionInputData.json'),
+                     self.resourcePath('SampleDataDescription/DataImporterInputData.json'),
+                     self.resourcePath('SampleDataDescription/SPHARM-PDMTestData.json'),]
+
+        for json_file in json_list:
+            with open(json_file, 'r') as f:
+                source_data = json.load(f)
+                #Register sample datasets for each module
+                iconPath = os.path.join(os.path.dirname(__file__).replace('\\','/'), 'Resources','Icons')
+                iconPath = None
+                #Data Importer sample dataset registration
+                SampleData.SampleDataLogic.registerCustomSampleDataSource(
+                  category=source_data['category'], sampleName=source_data['sampleName'],
+                  uris=source_data['uris'],
+                  fileNames=source_data['fileNames'],
+                  nodeNames=None,
+                  thumbnailFileName=iconPath,
+                  loadFileType=None,
+                  customDownloader = self.DownloadSampleDataInFolder,
+                  checksums = source_data['checksums']
+                )
+
     def slot(self):
         pos = self.HomeTextSection.textCursor().position()
 
@@ -101,3 +125,83 @@ The drop-down Modules are ordered to follow the basic workflow for choosing and 
             slicer.util.selectModule(slicer.moduleNames.RegressionComputation)
         elif pos >= 758 and pos <= 774 :
              slicer.util.selectModule(slicer.moduleNames.Home) #MFSDA
+
+
+    def DownloadSampleDataInFolder(self,source):
+        print(self)
+        destFolderPath = str(qt.QFileDialog.getExistingDirectory())
+
+        if not os.path.isdir(destFolderPath):
+            print('Invalid path %s' %{destFolderPath})
+            print("Can't download the sample data")
+            return
+
+        if not hasattr(source,'checksums'):
+            checksums = [None] * len(source.uris)
+        else:
+            checksums = source.checksums
+
+        for uri, fileName, checksum  in zip(source.uris,source.fileNames,checksums):
+            self.downloadFile(uri, destFolderPath, fileName, checksum=checksum)
+
+        print('Data folder: %s' % destFolderPath)
+
+    def reportHook(self,blocksSoFar,blockSize,totalSize):
+        # we clamp to 100% because the blockSize might be larger than the file itself
+        percent = min(int((100. * blocksSoFar * blockSize) / totalSize), 100)
+        if percent == 100 or (percent - self.downloadPercent >= 10):
+          # we clamp to totalSize when blockSize is larger than totalSize
+          humanSizeSoFar = self.humanFormatSize(min(blocksSoFar * blockSize, totalSize))
+          humanSizeTotal = self.humanFormatSize(totalSize)
+          print('<i>Downloaded %s (%d%% of %s)...</i>' % (humanSizeSoFar, percent, humanSizeTotal))
+          self.downloadPercent = percent
+
+    def downloadFile(self, uri, destFolderPath, name, checksum=None):
+        """
+        :param uri: Download URL.
+        :param destFolderPath: Folder to download the file into.
+        :param name: File name that will be downloaded.
+        :param checksum: Checksum formatted as ``<algo>:<digest>`` to verify the downloaded file. For example, ``SHA256:cc211f0dfd9a05ca3841ce1141b292898b2dd2d3f08286affadf823a7e58df93``.
+        """
+        filePath = destFolderPath + '/' + name
+        (algo, digest) = extractAlgoAndDigest(checksum)
+        if not os.path.exists(filePath) or os.stat(filePath).st_size == 0:
+          import urllib.request, urllib.parse, urllib.error
+          print('<b>Requesting download</b> <i>%s</i> from %s ...' % (name, uri))
+          # add a progress bar
+          self.downloadPercent = 0
+          try:
+            urllib.request.urlretrieve(uri, filePath, self.reportHook)
+            print('<b>Download finished</b>')
+          except IOError as e:
+            print('<b>\tDownload failed: %s</b>' % e, logging.ERROR)
+
+          if algo is not None:
+            print('<b>Verifying checksum</b>')
+            current_digest = computeChecksum(algo, filePath)
+            if current_digest != digest:
+              print('<b>Checksum verification failed. Computed checksum %s different from expected checksum %s</b>' % (current_digest, digest))
+              qt.QFile(filePath).remove()
+            else:
+              print('<b>Checksum OK</b>')
+        else:
+          if algo is not None:
+            print('<b>Verifying checksum</b>')
+            current_digest = computeChecksum(algo, filePath)
+            if current_digest != digest:
+              print('<b>File already exists in cache but checksum is different - re-downloading it.</b>')
+              qt.QFile(filePath).remove()
+              return self.downloadFile(uri, destFolderPath, name, checksum)
+            else:
+              print('<b>File already exists and checksum is OK - reusing it.</b>')
+          else:
+            print('<b>File already exists in cache - reusing it.</b>')
+        return filePath
+
+    def humanFormatSize(self,size):
+        """ from http://stackoverflow.com/questions/1094841/reusable-library-to-get-human-readable-version-of-file-size"""
+        for x in ['bytes','KB','MB','GB']:
+          if size < 1024.0 and size > -1024.0:
+            return "%3.1f %s" % (size, x)
+          size /= 1024.0
+        return "%3.1f %s" % (size, 'TB')
